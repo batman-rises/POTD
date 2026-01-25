6c1a4c875036d8d2a48aee2b8d95c1e715239b62
Binayak Panda <pbinayak604@gmail.com>
2026-01-25 20:20:47 +0530

    jan 25

---
+++ LEETCODE/1984. Minimum Difference Between Highest and Lowest of K Scores.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumDifference(vector<int>& a, int k) {
+        int n=a.size();
+        if(n<=1)return 0;
+        sort(a.begin(),a.end());
+        int ans=1e8;
+        
+        for(int i=0;i<=n-k;i++){
+            ans=min(ans,a[i+k-1]-a[i]);
+        }
+
+
+        return ans;
+    }
+};
