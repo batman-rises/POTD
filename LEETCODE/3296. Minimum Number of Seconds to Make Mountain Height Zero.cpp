bb783fb431956a793f96a207c37a5c64a9113e18
Binayak Panda <pbinayak604@gmail.com>
2026-03-13 20:57:14 +0530

    Mar 13, 2026

---
+++ LEETCODE/3296. Minimum Number of Seconds to Make Mountain Height Zero.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    typedef long long ll;
+    bool check(ll mid,vector<int>& workerTimes,int mountainHeight){
+        ll h=0;
+        for(int &t:workerTimes){
+            h += (ll)(sqrt(2.0 * mid/t + 0.25) - 0.5);
+            if(h>=mountainHeight){
+                return true;
+            }
+        }
+        return (h>=mountainHeight);
+    }
+    long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
+        int maxTime = *max_element(begin(workerTimes), end(workerTimes));
+        ll l = 1;
+        ll r = (ll)maxTime * mountainHeight * (mountainHeight+1)/2;
+
+        ll res=0;
+        while(l<=r){
+            ll mid=l+(r-l)/2;
+            if(check(mid,workerTimes,mountainHeight)){
+                res=mid;
+                r=mid-1;
+            }else{
+                l=mid+1;
+            }
+        }
+        return res;
+    }
+};
