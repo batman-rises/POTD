f4fd96139ff56147b98a0544512161075d2eb938
Binayak Panda <pbinayak604@gmail.com>
2026-03-22 22:38:51 +0530

    mar 22, 2026

---
+++ LEETCODE/EASY/1886. Determine Whether Matrix Can Be Obtained By Rotation.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean findRotation(int[][] mat, int[][] target) {
+        for(int i = 0; i < 4; i++) {
+            if(equal(mat, target)) return true;
+            mat = rotate(mat);
+        }
+        return false;
+    }
+
+    private int[][] rotate(int[][] mat) {
+        int n = mat.length;
+        int[][] res = new int[n][n];
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                res[j][n - 1 - i] = mat[i][j];
+            }
+        }
+        return res;
+    }
+
+    private boolean equal(int[][] a, int[][] b) {
+        int n = a.length;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(a[i][j] != b[i][j])
+                    return false;
+            }
+        }
+        return true;
+    }
+}
